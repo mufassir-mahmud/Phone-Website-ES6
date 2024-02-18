f1bd0a30974c12773e9e755e6aa29cbf7d941221
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-02-18 10:20:02 +0600

    added phones

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const loadData = async () =>{
+const loadData = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    displayPhones(phones)
 }
-    loadData()
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById("phone-container")
+    phones.forEach(phone => {
+        console.log(phone);
+        // step 2
+        const phoneCard = document.createElement('div');
+        // step 3
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-center ">
+        <button class="btn btn-primary ">Buy Now</button>
+        </div>                  
+        </div>                     
+        
+        `;
+        phoneContainer.appendChild(phoneCard)
+    })
+}
+loadData()
